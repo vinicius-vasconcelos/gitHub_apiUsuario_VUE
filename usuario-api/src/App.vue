eb0febacb3f96e54259d541272f1d433657b1dc9
Vinicius-Vasconcelos <viniciussouzav@gmail.com>
2020-03-07 23:10:13 -0300

    Finalizando brincadeira

---
+++ usuario-api/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+	<div id="app">
+		<NavBarMenu />
+		<MainData />
+	</div>
+</template>
+
+<script>
+	import NavBarMenu from '@/components/NavBarMenu.vue'
+	import MainData from '@/components/MainData.vue'
+
+	export default {
+		name: 'App',
+		components: {
+			NavBarMenu,
+			MainData
+		}
+	}
+</script>
+
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+	}
+
+	html, body {
+		width: 100%;
+		height: 100%;
+	}
+
+	body { background: #EFEFEF; }
+
+	#app {
+		width: 100%;
+		height: 100vh;
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		color: #24292E;
+		font-size: 16px;
+	}
+</style>
